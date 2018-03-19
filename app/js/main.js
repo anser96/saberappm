311e3261689b7b1b99320b6dded90287e6f705f8
Unknown <ljaramillonegrete@gmail.com>
2018-03-19 08:16:18 -0500

    Agregado modales

--- app/js/main.js
+++ app/js/main.js
@@ -1,48 +1,76 @@
+$(document).ready(function(){    
+    $('.modal').modal({
+      dismissible: false
+    });
+
+    $('#env').click(sinResponder);
+});
+
 var numQuestions = 36;
 var numOptions = 4;
 var rawScore = 0;
 var actualScore = 0;
-var sinResponder = 0;
 var answers = [
-                3, 0, 2, 2, 1, 0, 0, 1, 3, 2,
-                2, 1, 1, 0, 0, 3, 2, 3, 2, 0,
-                3, 3, 2, 1, 0, 2, 0, 1, 3, 2,
                 0, 1, 3, 3, 3, 0
               ];
+// var answers = [
+//                 3, 0, 2, 2, 1, 0, 0, 1, 3, 2,
+//                 2, 1, 1, 0, 0, 3, 2, 3, 2, 0,
+//                 3, 3, 2, 1, 0, 2, 0, 1, 3, 2,
+//                 0, 1, 3, 3, 3, 0
+//               ];
 
-function getAnswer() {
+function getAnswers() {
   var question;
-  var noCheck = 0;
-  for (var i = 0; i < numQuestions; i++) {
+  for (var i = 30; i < numQuestions; i++) {
     question = document.getElementsByName(i);
     for (var j=0; j < numOptions; j++) {
       if (question[j].checked) {
-        if (question[j].value == answers[i]) {
+        if (question[j].value == answers[i-30]) {
           rawScore++;
         }
-      } else {
+      }
+    }
+  }
+}
+
+var sinResponder = function() {
+  var m = document.getElementById("sinResponder");
+  var question;
+  var sinResp = 0;
+  var noCheck = 0;
+  for (var i = 30; i < numQuestions; i++) {
+    question = document.getElementsByName(i);
+    for (var j=0; j < numOptions; j++) {
+      if (!question[j].checked) {
         noCheck++;
       }
     }
     if (noCheck == numOptions) {
-      sinResponder++;
+      sinResp++;
     }
     noCheck = 0;
+  }
+
+  console.log("sinResponder: " + sinResp);
+  if (sinResp > 0) {
+    m.innerHTML = "Parece que aún tienes preguntas sin responder.";
+  } else {
+    m.innerHTML = "";
   }
 }
 
 function calcScore() {
-  getAnswer();
-  actualScore =  Math.round(rawScore / numQuestions * 100);
-  console.log("sinResponder: " + sinResponder);
+  var c = document.getElementById("puntaje");
+  getAnswers();
+  actualScore =  Math.round(rawScore / 6 * 100);
   console.log("puntaje: " + actualScore);
-  document.getElementById("quizForm").enviar.disabled = true;
-  var s = document.getElementById("calificacion");
   var msg_str ="Su puntuación es %d%%";
   msg_str = msg_str.replace("%d",actualScore).replace("%%","%");
-  if (s) {
-    s.innerHTML = "<div class='feedback'><p>"+msg_str+"</p></div>";
-  } else {
-    alert(msg_str);
-  }
+
+  c.innerHTML = msg_str;
+  $('#modal2').modal('open');
+  $('#env').addClass('hide');
+  $('#punt').removeClass('hide');
+  // document.getElementById("quizForm").enviar.disabled = true;
 }
