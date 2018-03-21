cc291ec730c4a41feae8e5b38c550cd23597f64a
Unknown <ljaramillonegrete@gmail.com>
2018-03-21 01:39:00 -0500

    Beta

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,52 @@
-$(document).ready(function(){    
+$(document).ready(function(){
     $('.modal').modal({
       dismissible: false
     });
 
     $('#env').click(sinResponder);
 });
-
 var numQuestions = 36;
 var numOptions = 4;
 var rawScore = 0;
 var actualScore = 0;
-var answers = [
-                0, 1, 3, 3, 3, 0
-              ];
 // var answers = [
-//                 3, 0, 2, 2, 1, 0, 0, 1, 3, 2,
-//                 2, 1, 1, 0, 0, 3, 2, 3, 2, 0,
-//                 3, 3, 2, 1, 0, 2, 0, 1, 3, 2,
 //                 0, 1, 3, 3, 3, 0
 //               ];
+var answers = [
+                3, 0, 2, 2, 1, 0, 0, 1, 3, 2,
+                2, 1, 1, 0, 0, 3, 2, 3, 2, 0,
+                3, 3, 2, 1, 0, 2, 0, 1, 3, 2,
+                0, 1, 3, 3, 3, 0
+              ];
+var selections = [];
 
 function getAnswers() {
   var question;
-  for (var i = 30; i < numQuestions; i++) {
+  var selection;
+  var noCheck = 0;
+
+  for (var i = 0; i < numQuestions; i++) {
     question = document.getElementsByName(i);
     for (var j=0; j < numOptions; j++) {
       if (question[j].checked) {
-        if (question[j].value == answers[i-30]) {
+        if (question[j].value == answers[i]) {
           rawScore++;
         }
+        selection = question[j].value;
+        break;
+      } else {
+        noCheck++;
       }
     }
+
+    if (noCheck == numOptions) {
+      selections.push("nr");
+    } else {
+      selections.push(selection);
+    }
+    noCheck = 0;
   }
+  console.log(selections);
 }
 
 var sinResponder = function() {
@@ -39,7 +54,7 @@ var sinResponder = function() {
   var question;
   var sinResp = 0;
   var noCheck = 0;
-  for (var i = 30; i < numQuestions; i++) {
+  for (var i = 0; i < numQuestions; i++) {
     question = document.getElementsByName(i);
     for (var j=0; j < numOptions; j++) {
       if (!question[j].checked) {
@@ -62,15 +77,42 @@ var sinResponder = function() {
 
 function calcScore() {
   var c = document.getElementById("puntaje");
+  var row ="";
+  var a;
+  var s;
+
   getAnswers();
-  actualScore =  Math.round(rawScore / 6 * 100);
+  actualScore =  Math.round(rawScore / numQuestions * 100);
   console.log("puntaje: " + actualScore);
   var msg_str ="Su puntuación es %d%%";
   msg_str = msg_str.replace("%d",actualScore).replace("%%","%");
 
   c.innerHTML = msg_str;
+
+  for (var i = 0; i < numQuestions; i++) {
+    a = answers[i] == 0 ? 'A' : answers[i] == 1 ? 'B' : answers[i] == 2 ? 'C' : 'D';
+    s = selections[i] == 0 ? 'A' : selections[i] == 1 ? 'B' : selections[i] == 2 ? 'C' : selections[i] == 3 ? 'D' : 'No respondió';
+    if (a == s) {
+      row += `
+      <tr class="green">
+      <td>${i+1}</td>
+      <td>${a}</td>
+      <td>${s}</td>
+      </tr>
+      `;
+    } else {
+      row += `
+      <tr class="red">
+      <td>${i+1}</td>
+      <td>${a}</td>
+      <td>${s}</td>
+      </tr>
+      `;
+    }
+  }
+
+  $('#tabla tbody').append(row);
   $('#modal2').modal('open');
   $('#env').addClass('hide');
   $('#punt').removeClass('hide');
-  // document.getElementById("quizForm").enviar.disabled = true;
 }
