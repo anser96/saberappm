0829d058af1891e53a3cf46d928abc7f2622d4a7
Unknown <ljaramillonegrete@gmail.com>
2018-03-22 22:23:02 -0500

    Agregado paginacion

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+  $("#pasos").steps({
+    headerTag: "h3",
+    bodyTag: "section",
+    transitionEffect: "fade",
+    autoFocus: true,
+    
+    onStepChanged: function (event, currentIndex, priorIndex) {
+      $('body,html').animate({scrollTop : 0}, 500);
+    },
+
+    labels: {
+      finish: "Fin",
+      next: "Siguiente",
+      previous: "Anterior"
+    }
+  });
+  $('.steps').addClass('hide');
+  $('.actions ul li a').addClass('waves-effect waves-light btn colorb');
     $('.modal').modal({
       dismissible: false
     });
@@ -9,9 +27,6 @@ var numQuestions = 36;
 var numOptions = 4;
 var rawScore = 0;
 var actualScore = 0;
-// var answers = [
-//                 0, 1, 3, 3, 3, 0
-//               ];
 var answers = [
                 3, 0, 2, 2, 1, 0, 0, 1, 3, 2,
                 2, 1, 1, 0, 0, 3, 2, 3, 2, 0,
